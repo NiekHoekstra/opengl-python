9ba606c7a59f7304e6e886d4ffe8eb4655a9cfab
Niek Hoekstra <niekhoekstra@gmail.com>
2025-12-29 18:07:35 +0100

    fix(triangle): Removed old framework code.

--- src/demos/01_triangle.py
+++ src/demos/01_triangle.py
@@ -1,18 +1,15 @@
 import ctypes
-from functools import partial
 
 import numpy as np
 import pygame.time
 
-from framework.easy import Easy
+from framework.windows import GLWindow
 
 vs = """#version 330 core
 layout (location = 0) in vec3 aPos;
-out vec3 iPos;
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
-    iPos = aPos;
 }
 """
 
@@ -33,10 +30,6 @@ import pygame
 from OpenGL import GL
 
 
-def _render(vao: int, vbo: int):
-    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
-
-
 def compile_shader(gl_type, source) -> int:
     shader = GL.glCreateShader(gl_type)
     GL.glShaderSource(shader, source)
@@ -45,7 +38,7 @@ def compile_shader(gl_type, source) -> int:
     if result != 1:
         error = GL.glGetShaderInfoLog(shader)
         if isinstance(error, bytes):
-            error = error.decode('utf-8')
+            error = error.decode("utf-8")
         raise RuntimeError(error)
     return shader
 
@@ -60,45 +53,52 @@ def compile_program():
     if result != 1:
         error = GL.glGetProgramInfoLog(p)
         if isinstance(error, bytes):
-            error = error.decode('utf-8')
+            error = error.decode("utf-8")
         raise RuntimeError(error)
     return p
 
 
 def main():
-    demo = Easy()
-    demo.setup()
-    # ---
-    vao = GL.glGenVertexArrays(1)
-    vbo = GL.glGenBuffers(1)
-    # ---
-    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
-    GL.glBufferData(
-        GL.GL_ARRAY_BUFFER,  # target
-        vertices.nbytes,  # size
-        vertices,  # data
-        GL.GL_STATIC_DRAW  # usage
-    )
-    GL.glBindVertexArray(vao)
-    GL.glEnableVertexAttribArray(0)
-    GL.glVertexAttribPointer(
-        0,  # index
-        3,  # size
-        GL.GL_FLOAT,  # type
-        GL.GL_FALSE,  # normalized
-        3 * 4,  # stride
-        ctypes.c_void_p(0),  # pointer
-    )
-    # ---
-    p = compile_program()
-    GL.glUseProgram(p)
-    #
-    demo.loop(
-        None,
-        partial(_render, vao, vbo)
-    )
-    # Cleanup, a formality.
-    pygame.quit()
+    with GLWindow() as window:
+        vao = GL.glGenVertexArrays(1)
+        vbo = GL.glGenBuffers(1)
+        # ---
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
+        GL.glBufferData(
+            GL.GL_ARRAY_BUFFER,  # target
+            vertices.nbytes,  # size
+            vertices,  # data
+            GL.GL_STATIC_DRAW,  # usage
+        )
+        GL.glBindVertexArray(vao)
+        GL.glEnableVertexAttribArray(0)
+        GL.glVertexAttribPointer(
+            0,  # index
+            3,  # size
+            GL.GL_FLOAT,  # type
+            GL.GL_FALSE,  # normalized
+            3 * 4,  # stride
+            ctypes.c_void_p(0),  # pointer
+        )
+        # ---
+        p = compile_program()
+        running = True
+        while running:
+            window.clear()
+            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
+            GL.glBindVertexArray(vao)
+            GL.glUseProgram(p)
+            GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+
+            window.flip()
+            pygame.time.wait(33)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
 
 
 if __name__ == "__main__":
