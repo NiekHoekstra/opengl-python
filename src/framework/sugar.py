14c022c4011aed924799a9ce703c5a53abc4331f
Niek Hoekstra <niekhoekstra@gmail.com>
2025-12-29 16:26:58 +0100

    feat(sugar): VertexData system.

---
+++ src/framework/sugar.py
@@ -0,0 +1,69 @@
+"""
+Sugar Syntax for *some* clarity.
+"""
+
+__all__ = ["VertexData"]
+
+from typing import Self
+
+import numpy as np
+
+
+class VertexData:
+    """
+    VertexData Object for sugar syntax.
+    """
+
+    def __init__(self):
+        self._data = []
+
+    def xyz(self, x: float, y: float, z: float = 0.0) -> Self:
+        """
+        Add XYZ point to the internal buffer.
+        :param x: X coordinate.
+        :param y: Y coordinate.
+        :param z: Z coordinate.
+        :return: Self
+        """
+        self._data.extend((x, y, z))
+        return self
+
+    def rgb(self, r: float, g: float, b: float) -> Self:
+        """
+        Add an RGB color to the internal buffer.
+        :param r: Red (0.0-1.0)
+        :param g: Green (0.0-1.0)
+        :param b: Blue (0.0-1.0)
+        :return: Self
+        """
+        self._data.extend((r, g, b))
+        return self
+
+    def rgba(self, r: float, g: float, b: float, a: float = 1.0) -> Self:
+        """
+        Add an RGBA color to the internal buffer.
+        :param r: Red (0.0-1.0)
+        :param g: Green (0.0-1.0)
+        :param b: Blue (0.0-1.0)
+        :param a: Alpha (0.0-1.0)
+        :return: Self
+        """
+        self._data.extend((r, g, b, a))
+        return self
+
+    def uv(self, u: float, v: float) -> Self:
+        """
+        Add UV (texture) coordinates to the internal buffer.
+        :param u: U coordinate.
+        :param v: V coordinate.
+        :return: Self
+        """
+        self._data.extend((u, v))
+        return self
+
+    def compile(self):
+        """
+        Compile the data to array and return it.
+        :return: Data.
+        """
+        return np.array(self._data, np.float32)
